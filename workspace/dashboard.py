299546380cefb04c24b30f686d0de395e6c75d2d
web <1044573025@qq.com>
2019-04-12 21:15:08 +0800

    BI

--- workspace/dashboard.py
+++ workspace/dashboard.py
@@ -17,11 +17,21 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
     api_mapping = {
         "organtree": 'self.get_organtree',
         "facet": 'self.get_get_facet',
+        # 团队幸福温度
         "temperature": "self.get_temperature",
+        # 团队幸福指数整体特征
         "feature": "self.get_feature",
+        # 员工幸福与压力/敬业整体分布
         "distribution": "self.get_distribution",
+        # 下属机构幸福指数表现 && 幸福指数各维度特征
         "expression": "self.get_expression",
+        # 幸福指数关注群体
+        "group": "self.get_focus_group",
+        # 员工幸福整体分布
+        "overall": "self.get_overall",
+        "": "self.get_",
     }
+
 
     def get_temperature(self, **kwargs):
         res = {}
@@ -48,12 +58,20 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
             return res, ErrorCode.INVALID_INPUT
         try:
             query_dict = self.get_organization(org)[0]
-            res = FactOEI.objects.complex_filter(query_dict).aggregate(
-                Avg("model"), Avg("scale2"), Avg("dimension1"), Avg("dimension2"),
-                Avg("dimension3"), Avg("dimension4"), Avg("dimension5"), Avg("dimension6")
+            res2 = FactOEI.objects.complex_filter(query_dict).aggregate(
+                Avg("model"), Avg("scale2"), Avg("dimension1"), Avg("dimension2"), Avg("dimension3"),
+                Avg("dimension4"), Avg("dimension5"), Avg("dimension6"), Avg("dimension7")
             )
-            if res:
-                res = {i: round(res[i]) for i in res}
+            if res2:
+                res[u"企业幸福指数"] = round(res2["model__avg"])
+                res[u"压力指数"] = round(res2["scale2__avg"])
+                res[u"工作投入"] = round(res2["dimension1__avg"])
+                res[u"生活愉悦"] = round(res2["dimension2__avg"])
+                res[u"成长有力"] = round(res2["dimension3__avg"])
+                res[u"人际和谐"] = round(res2["dimension4__avg"])
+                res[u"领导激发"] = round(res2["dimension5__avg"])
+                res[u"组织卓越"] = round(res2["dimension6__avg"])
+                res[u"员工幸福能力"] = round(res2["dimension7__avg"])
                 return res, ErrorCode.SUCCESS
             else:
                 return res, ErrorCode.NOT_EXISTED
@@ -192,7 +210,7 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
                         else:
                             res["r7c7"] += 1
                 for i in res:
-                    res[i] = round(res[i]*100/total, 2)
+                    res[i] = round(res[i] * 100 / total, 2)
                 return res, ErrorCode.SUCCESS
             else:
                 return res, ErrorCode.NOT_EXISTED
@@ -204,6 +222,7 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
         res = {}
         org = kwargs.get("org_id")
         dimension = kwargs.get("dimension_id")
+        select = kwargs.get("select_id")
         if not org:
             return res, ErrorCode.INVALID_INPUT
         try:
@@ -222,28 +241,8 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
             else:
                 company = FactOEI.objects.complex_filter(query_dict).aggregate(Avg("model"), Avg("scale2"))
             res[org_list[-1]] = company
-            #
-            len_query = len(query_dict)
-            if len_query == 1:
-                child_org = FactOEI.objects.complex_filter(query_dict).values_list("organization1", "organization2").distinct()
-            elif len_query == 2:
-                child_org = FactOEI.objects.complex_filter(query_dict).values_list(
-                    "organization1", "organization2", "organization3"
-                ).distinct()
-            elif len_query == 3:
-                child_org = FactOEI.objects.complex_filter(query_dict).values_list(
-                    "organization1", "organization2", "organization3", "organization4"
-                ).distinct()
-            elif len_query == 4:
-                child_org = FactOEI.objects.complex_filter(query_dict).values_list(
-                    "organization1", "organization2", "organization3", "organization4", "organization5"
-                ).distinct()
-            elif len_query == 5:
-                child_org = FactOEI.objects.complex_filter(query_dict).values_list(
-                    "organization1", "organization2", "organization3", "organization4", "organization5", "organization6"
-                ).distinct()
-            else:
-                return {}, ErrorCode.INVALID_INPUT
+            # 查下属机构
+            child_org = self.get_child_org(query_dict)
             if child_org:
                 child_list = []
                 for tpl in child_org:
@@ -262,15 +261,45 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
                             u"组织卓越": "dimension6",
                             u"员工幸福能力": "dimension7",
                         }
-                        child_depart = FactOEI.objects.complex_filter(child_query_dict).aggregate(Avg(dimension_dict[dimension]))
+                        child_depart = FactOEI.objects.complex_filter(child_query_dict).aggregate(
+                            Avg(dimension_dict[dimension]))
                     else:
-                        child_depart = FactOEI.objects.complex_filter(child_query_dict).aggregate(Avg("model"), Avg("scale2"))
+                        child_depart = FactOEI.objects.complex_filter(child_query_dict).aggregate(Avg("model"),
+                                                                                                  Avg("scale2"))
                     res[child_org_list[-1]] = child_depart
 
             if res:
+                head, name, score, score2 = {}, [], [], []
                 for dic in res:
-                    for item in res[dic]:
-                        res[dic][item] = round(res[dic][item])
+                    if dic == org_list[-1]:
+                        if not dimension:
+                            head = {dic: [round(res[dic]["model__avg"]), round(res[dic]["scale2__avg"])]}
+                        else:
+                            head = {dic: round(tuple(res[dic].values())[0])}
+
+                    else:
+                        name.append(dic)
+                        if not dimension:
+                            score.append(round(res[dic]["model__avg"]))
+                            score2.append(round(res[dic]["scale2__avg"]))
+                        else:
+                            score.append(round(tuple(res[dic].values())[0]))
+                if dimension:
+                    result = [list(i) for i in zip(name, score)]
+                    result.sort(key=lambda x: x[1], reverse=True)
+                else:
+                    result = [list(i) for i in zip(name, score, score2)]
+                    if select:
+                        if select == 'high':
+                            result = [i for i in result if i[1] >= 70]
+                        if select == 'mid':
+                            result = [i for i in result if 60 < i[1] < 70]
+                        if select == 'low':
+                            result = [i for i in result if i[1] <= 60]
+                res = {
+                    "header": head,
+                    "body": result
+                }
 
                 return res, ErrorCode.SUCCESS
             else:
@@ -280,6 +309,102 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
             logger.error("get report data error, msg: %s " % e)
             return res, ErrorCode.INTERNAL_ERROR
 
+    def get_focus_group(self, **kwargs):
+        org = kwargs.get("org_id")
+        profile = kwargs.get("profile_id")
+        profile_dict = {u"年龄": "profile1", u"性别": "profile2", u"序列": "profile3", u"司龄": "profile4", u"层级": "profile5"}
+
+        query_dict, org_list = self.get_organization(org)
+        company_obj = FactOEI.objects.complex_filter(query_dict)
+        res = {}
+        if profile in profile_dict:
+            types = company_obj.values_list(profile_dict[profile]).distinct()
+            types = [i[0] for i in types]
+
+        else:
+            # 查下属部门
+            child_org = self.get_child_org(query_dict)
+            if child_org:
+                types = []
+                for tpl in child_org:
+                    j = '.'.join([i for i in tpl if i])
+                    if j not in types:
+                        types.append(j)
+            else:
+                types = None
+                return {}, ErrorCode.NOT_EXISTED
+        for i in types:
+            res[i] = {}
+            if profile in profile_dict:
+                query_dict = {profile_dict[profile]: i}
+            else:
+                query_dict = self.get_organization(i)[0]
+            type_obj = company_obj.complex_filter(query_dict)
+            res[i][u"人数"] = type_obj.count()
+            scores = type_obj.aggregate(Avg("dimension1"), Avg("dimension2"), Avg("dimension3"), Avg("dimension4"),
+                                        Avg("dimension5"), Avg("dimension6"), Avg("model"))
+            res[i][u"工作投入"] = round(scores["dimension1__avg"], 2)
+            res[i][u"生活愉悦"] = round(scores["dimension2__avg"], 2)
+            res[i][u"成长有力"] = round(scores["dimension3__avg"], 2)
+            res[i][u"人际和谐"] = round(scores["dimension4__avg"], 2)
+            res[i][u"领导激发"] = round(scores["dimension5__avg"], 2)
+            res[i][u"组织卓越"] = round(scores["dimension6__avg"], 2)
+            level = self.get_level(scores["model__avg"])
+            res[i][u"区间"] = level
+        return res, ErrorCode.SUCCESS
+
+    # 获取梯队区间
+    def get_level(self, score):
+        if 80 <= score <= 100:
+            level = u"优势区"
+        elif 75 <= score < 80:
+            level = u"保持区"
+        elif 70 <= score < 75:
+            level = u"潜力区"
+        elif 65 <= score < 70:
+            level = u"需提升"
+        elif 60 < score < 65:
+            level = u"提升区"
+        elif 40 < score <= 60:
+            level = u"改进区"
+        else:
+            level = u"障碍区"
+        return level
+
+    # 员工幸福整体分布
+    def get_overall(self, **kwargs):
+        org = kwargs.get("org_id")
+        query_dict = self.get_organization(org)[0]
+        company_query_set = FactOEI.objects.complex_filter(query_dict)
+        target = ["model", "scale2", "dimension1", "dimension2", "dimension3", "dimension4",
+                  "dimension5", "dimension6", "dimension7"]
+        ret = {}
+        for i in target:
+            ret[i] = {}
+            ret[i]["total"] = company_query_set.values_list(i)
+            ret[i][u"优势区"] = []
+            ret[i][u"保持区"] = []
+            ret[i][u"潜力区"] = []
+            ret[i][u"需提升"] = []
+            ret[i][u"提升区"] = []
+            ret[i][u"改进区"] = []
+            ret[i][u"障碍区"] = []
+            for j in ret[i]["total"]:
+                level = self.get_level(j[0])
+                ret[i][level].append(j[0])
+
+        for i in ret:
+            for j in ret[i]:
+                if j != "total":
+                    ret[i][j] = round(len(ret[i][j]) * 100 / len(ret[i]["total"]))
+        res = {}
+        res_zip = {u"企业幸福指数": "model", u"压力指数": "scale2", u"工作投入": "dimension1", u"生活愉悦": "dimension2",
+                   u"成长有力": "dimension3", u"人际和谐": "dimension4", u"领导激发": "dimension5",
+                   u"组织卓越": "dimension6", u"员工幸福能力": "dimension7"}
+        for i in res_zip:
+            res[i] = ret[res_zip[i]]
+        return res, ErrorCode.SUCCESS
+
     def get_organization(self, org):
         organization = (
             'organization1', 'organization2', 'organization3', 'organization4', 'organization5', 'organization6'
@@ -287,6 +412,31 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
         org_list = org.split('.')
         query_dict = dict(zip(organization, org_list))
         return query_dict, org_list
+
+    def get_child_org(self, query_dict):
+        len_query = len(query_dict)
+        if len_query == 1:
+            child_org = FactOEI.objects.complex_filter(query_dict).values_list("organization1",
+                                                                               "organization2").distinct()
+        elif len_query == 2:
+            child_org = FactOEI.objects.complex_filter(query_dict).values_list(
+                "organization1", "organization2", "organization3"
+            ).distinct()
+        elif len_query == 3:
+            child_org = FactOEI.objects.complex_filter(query_dict).values_list(
+                "organization1", "organization2", "organization3", "organization4"
+            ).distinct()
+        elif len_query == 4:
+            child_org = FactOEI.objects.complex_filter(query_dict).values_list(
+                "organization1", "organization2", "organization3", "organization4", "organization5"
+            ).distinct()
+        elif len_query == 5:
+            child_org = FactOEI.objects.complex_filter(query_dict).values_list(
+                "organization1", "organization2", "organization3", "organization4", "organization5", "organization6"
+            ).distinct()
+        else:
+            child_org = None
+        return child_org
 
     def get_organtree(self,**kargs):
         """return organization tree list"""
@@ -355,30 +505,30 @@ class Dashboard(AuthenticationExceptView, WdListCreateAPIView):
         return []
 
     def post(self, request, *args, **kwargs):
-        
         api_id = self.request.data.get("api", None)
         org_id = self.request.data.get("org", None)
         profile_id = self.request.data.get("profile", None)
         dimension_id = self.request.data.get("dimension", None)
         population_id = self.request.data.get("population", None)
         scale_id = self.request.data.get("scale", None)
-
+        select_id = self.request.data.get("select", None)
         args = {"org_id": org_id,
                 "profile_id": profile_id,
                 "dimension_id": dimension_id,
                 "population_id": population_id}
-        
         try:
-            #retrieve chart's data
+            # retrieve chart's data
+
             data, err_code = eval(self.api_mapping[api_id])(org_id=org_id,
                                                             profile_id=profile_id,
                                                             dimension_id=dimension_id,
                                                             population_id=population_id,
-                                                            scale_id=scale_id)
+                                                            scale_id=scale_id,
+                                                            select_id=select_id, )
             if err_code != ErrorCode.SUCCESS:
                 return general_json_response(status.HTTP_200_OK, ErrorCode.INVALID_INPUT, {"msg": err_code})
             else:
                 return general_json_response(status.HTTP_200_OK, ErrorCode.SUCCESS, {"data": data})
         except Exception, e:
             logger.error("dashboard error, msg is %s" % e)
-            return general_json_response(status.HTTP_200_OK, ErrorCode.INTERNAL_ERROR, {"msg": "%s" %e})
+            return general_json_response(status.HTTP_200_OK, ErrorCode.INTERNAL_ERROR, {"msg": "%s" % e})
