b9fe77688fa546726424560c28f3e199bab0d27f
vscode <gujiachao@gelue.com>
2019-07-03 11:12:49 +0800

    bugfix

--- useroperate/serializers.py
+++ useroperate/serializers.py
@@ -31,7 +31,7 @@ class TrialAssessListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AssessProject
-        fields = ('id','name','begin_time','end_time','organizations','joined','url')
+        fields = ('id','name','begin_time','end_time','organizations','joined')
     
     def get_organizations(self, obj):
         organizations = AssessJoinedOrganization.objects.filter(assess_id=obj.id,organization__parent_id__gt=0).values_list('organization__name', flat=True)
