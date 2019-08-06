5c3cdf0e452421297488a599b88d9f0eab39b5ca
vscode <gujiachao@gelue.com>
2019-08-06 11:27:17 +0800

    serializer application

--- application/serializers.py
+++ application/serializers.py
@@ -3,14 +3,13 @@ from __future__ import unicode_literals
 from rest_framework import serializers
 from application import models
 
-class ApplicationSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = models.Application
-        fields = '__all__'
-        depth = 1
+
 
 class ApplierSerializer(serializers.ModelSerializer):
+
+    size_text = serializers.CharField(source='get_size_display')
+    industry = serializers.CharField(source='get_industry_display')
+    source = serializers.CharField(source='get_source_display')
     
     class Meta:
         model = models.Applier
@@ -21,3 +20,13 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Event
         fields = '__all__'
+
+class ApplicationSerializer(serializers.ModelSerializer):
+    
+    progress_text = serializers.CharField(source='get_progress_display')
+    applier =  ApplierSerializer()
+
+    class Meta:
+        model = models.Application
+        fields = '__all__'
+        depth = 1
