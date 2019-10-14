d7089248f0fdac35bf8b48ce620e0f9a7c970ffb
vscode <gujiachao@gelue.com>
2019-10-14 18:32:39 +0800

    url

--- front/template_urls.py
+++ front/template_urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     url(r"^join-survey/$", OpenSurveyJoinView.as_view(), name="survey-join-view"),
     # 加入项目
     url(r"^join-project/$", OpenProjectJoinView.as_view(), name="project-join-view"),
-    # 个人链接地址
-    url(r"^(?P<personal_link>\w+)/$", LinkProjectJoinView.as_view(), name="project-join-view"),
     # Open Join Project
     url(r"^openjoin-project/$", OpenJoinView.as_view(), name="project-openjoin-view"),
     # open anonymous survey
-    url(r"^anonymous/$", AnonymousJoinView.as_view(), name="project-anonymous-view"),
+    url(r"^anonymous/$", AnonymousJoinView.as_view(), name="project-anonymous-view"),    
+    # 个人链接地址
+    url(r"^(?P<personal_link>\w+)/$", LinkProjectJoinView.as_view(), name="project-xxx-view"),
 ]
